151f328fa32714dbb6067039b7431e076215f734
Constantine <linetski@gmail.com>
2021-04-24 19:42:39 +0300

    enable eureka client

--- src/main/java/propets/lostAndFound/LostAndFoundApplication.java
+++ src/main/java/propets/lostAndFound/LostAndFoundApplication.java
@@ -2,8 +2,10 @@ package propets.lostAndFound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan()
 public class LostAndFoundApplication {
