a9eb819f733b1cd9cb94ac638c269d24e1db917f
Constantine <linetski@gmail.com>
2021-04-25 23:04:55 +0300

    ServiceInstanceRestController added

--- src/main/java/propets/lostAndFound/LostAndFoundApplication.java
+++ src/main/java/propets/lostAndFound/LostAndFoundApplication.java
@@ -1,9 +1,17 @@
 package propets.lostAndFound;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -13,5 +21,17 @@ public class LostAndFoundApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LostAndFoundApplication.class, args);
 	}
+	
+	@RestController
+	class ServiceInstanceRestController {
 
+		@Autowired
+		private DiscoveryClient discoveryClient;
+
+		@RequestMapping("/service-instances/{applicationName}")
+		public List<ServiceInstance> serviceInstancesByApplicationName(
+				@PathVariable String applicationName) {
+			return this.discoveryClient.getInstances(applicationName);
+		}
+	}
 }
