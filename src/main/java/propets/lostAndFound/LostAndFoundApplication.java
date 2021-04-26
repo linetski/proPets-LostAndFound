b11b1e6ed5e75d54f55597eced2b69b5ce193d9f
Constantine <linetski@gmail.com>
2021-04-26 15:15:24 +0300

    env test

--- src/main/java/propets/lostAndFound/LostAndFoundApplication.java
+++ src/main/java/propets/lostAndFound/LostAndFoundApplication.java
@@ -3,12 +3,15 @@ package propets.lostAndFound;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @ComponentScan()
 public class LostAndFoundApplication {
-
+	
+	@Value("${DOMAIN_NAME}")
+	private String domainName;
+	
+	@Value("${EUREKA_URL}")
+	private String url;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LostAndFoundApplication.class, args);
 	}
@@ -33,5 +42,11 @@ public class LostAndFoundApplication {
 				@PathVariable String applicationName) {
 			return this.discoveryClient.getInstances(applicationName);
 		}
+	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() {
+	    System.out.println(domainName);
+	    System.out.println(url);
 	}
 }
