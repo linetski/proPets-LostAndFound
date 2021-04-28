c0bbcbbdfba6d51077eac31d760307a5e556fdd7
Constantine <linetski@gmail.com>
2021-04-28 20:56:53 +0300

    prod

--- src/main/java/propets/lostAndFound/LostAndFoundApplication.java
+++ src/main/java/propets/lostAndFound/LostAndFoundApplication.java
@@ -10,44 +10,41 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan()
 public class LostAndFoundApplication {
 	
-	@Value("${DOMAIN_NAME}")
-	private String domainName;
-	
-	@Value("${eureka.client.serviceUrl.defaultZone}")
-	private String url;
+	/*
+	 * @Value("${DOMAIN_NAME}") private String domainName;
+	 * 
+	 * @Value("${eureka.client.serviceUrl.defaultZone}") private String url;
+	 */
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LostAndFoundApplication.class, args);
 	}
 	
-	@RestController
-	class ServiceInstanceRestController {
-
-		@Autowired
-		private DiscoveryClient discoveryClient;
-
-		@RequestMapping("/service-instances/{applicationName}")
-		public List<ServiceInstance> serviceInstancesByApplicationName(
-				@PathVariable String applicationName) {
-			return this.discoveryClient.getInstances(applicationName);
-		}
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
 	}
 	
 	@EventListener(ApplicationReadyEvent.class)
 	public void doSomethingAfterStartup() {
-	    System.out.println(domainName);
-	    System.out.println(url);
+		/*
+		 * System.out.println(domainName); System.out.println(url);
+		 */
 	    
 	}
 }
---
+++ src/main/java/propets/lostAndFound/filters/AuthFilter.java
@@ -0,0 +1,60 @@
+package propets.lostAndFound.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+
+@Component
+public class AuthFilter extends OncePerRequestFilter{
+	
+	private static final Logger logger = LoggerFactory.getLogger(AuthFilter.class);
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		logger.info("entered");
+		String token = (String) request.getHeader("Authorization");
+		logger.info("token: " + token);
+
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    headers.add("Authorization", token);
+		HttpEntity<String> requestObject = 
+			      new HttpEntity<String>(null, headers);
+	    
+		ResponseEntity<Boolean> responseEntity = 
+				restTemplate.exchange("http://propets-auth-service/auth/authenticate", HttpMethod.GET, requestObject, Boolean.class);
+
+		logger.info("response from auth: " + responseEntity.toString());
+		filterChain.doFilter(request, response);
+	}
+
+
+
+}
