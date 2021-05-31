890b34be294778c489de2c3849bfba95655906d8
Constantine <linetski@gmail.com>
2021-05-31 17:27:50 +0300

    fix

--- src/main/java/propets/model/FoundPet.java
+++ src/main/java/propets/model/FoundPet.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.ToString;
 
